dfcad8ec95abcf498d036ba650e19d26f204f89a
Johan Althoff <teetow@gmail.com>
2023-02-18 12:59:12 +0100

    chore: install postCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}), // add options if needed
+      ],
+    },
+  },
 });
